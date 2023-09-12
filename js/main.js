53752cf5bd524abf717b7ec045b54a414c5f0782
zeezo <108765812+zeezo679@users.noreply.github.com>
2023-09-13 02:53:22 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 //get array of images
 let backgrounds = [
-    'url(../images/1.png)',
-    'url(../images/2.png)',
-    'url(../images/3.png)',
-    'url(../images/4.png)',
-    'url(../images/5.png)',
+    'url(/images/1.png)',
+    'url(/images/2.png)',
+    'url(/images/3.png)',
+    'url(/images/4.png)',
+    'url(/images/5.png)',
 ]
 
 let landingPage = document.querySelector(".landing-page")
